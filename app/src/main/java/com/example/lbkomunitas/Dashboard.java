d20eb2a35d784e8efd083648c8c5b89f6dcbc08d
LB Komunitas <lbkomunitas@gmail.com>
2019-06-28 14:35:34 +0700

    first 4

--- app/src/main/java/com/example/lbkomunitas/Dashboard.java
+++ app/src/main/java/com/example/lbkomunitas/Dashboard.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+//import android.view.View.OnClickListener;
+
 public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
     }
+
+
+    public void mLomba(View v)
+    {
+        Intent i = new Intent(Dashboard.this,Lomba.class);
+        startActivity(i);
+    }
+
+
+
+
+
+
 }
