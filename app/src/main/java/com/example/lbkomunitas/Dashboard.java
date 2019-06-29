0dd6a40612887fc52b19f4f3ccd1624fc2439a21
LB Komunitas <lbkomunitas@gmail.com>
2019-06-29 10:31:55 +0700

    20190629

--- app/src/main/java/com/example/lbkomunitas/Dashboard.java
+++ app/src/main/java/com/example/lbkomunitas/Dashboard.java
@@ -23,10 +23,33 @@ public class Dashboard extends AppCompatActivity {
 
     public void mLomba(View v)
     {
-        Intent i = new Intent(Dashboard.this,Lomba.class);
+        Intent i = new Intent(this,Lomba.class);
         startActivity(i);
     }
-
+	
+	public void mDiskusi(View va)
+    {
+        Intent ia = new Intent(this,Diskusi.class);
+        startActivity(ia);
+    }
+	
+	public void mHome(View vb)
+    {
+        Intent ib = new Intent(this,Dashboard.class);
+        startActivity(ib);
+    }
+	
+	public void mBerita(View vc)
+    {
+        Intent ic = new Intent(this,Berita.class);
+        startActivity(ic);
+    }
+	
+	public void mCari(View vd)
+    {
+        Intent id = new Intent(this,Pencarian.class);
+        startActivity(id);
+    }
 
 
 
--- app/src/main/java/com/example/lbkomunitas/Fragment2.java
+++ app/src/main/java/com/example/lbkomunitas/Fragment2.java
@@ -1,16 +1,15 @@
-package com.example.lbkomunitas;  
+package com.example.lbkomunitas;
+
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;  
 import android.view.ViewGroup;
 
-
 import androidx.fragment.app.Fragment;
 
-public class Fragment1 extends Fragment {
-
-
+public class Fragment2 extends Fragment {
+  
     @Override  
     public void onCreate(Bundle savedInstanceState) {  
         super.onCreate(savedInstanceState);  
@@ -19,8 +18,8 @@ public class Fragment1 extends Fragment {
     @Override  
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        /* Inflate the layout for this fragment*/
-        View view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+        /* Inflate the layout for this fragment */
+        View view = inflater.inflate(R.layout.fragment_fragment2, container, false);
         return view;
     }
 
